14721af99a6e571b2af84294bd57e5436905449e
Siroj Inadias <joris@saidani.org>
2023-07-18 17:29:10 +0200

    Update Wips.tsx

--- components/Wips/Wips.tsx
+++ components/Wips/Wips.tsx
@@ -3,37 +3,34 @@ import {Wip} from '../Wip'
 
 export const Wips: FunctionComponent = () => (
   <div className="flex flex-wrap md:grid-cols-3 lg:grid-cols-4 gap-4 my-4">
-    <Wip
-      title="Mad Foxes"
-      details={`About to mix upcoming Mad Foxes album !`}
-    />
+    <Wip title="Mad Foxes" details={`Mixing Mad Foxes' upcoming new album!`} />
     <Wip
       title="Elise Bourn"
-      details={`About to mix some songs on upcoming Nantes-based talented folk artist Elise Bourn !`}
+      details={`About to mix a few songs for the debut album of the talented, Nantes-based folk artist Elise Bourn!`}
     />
     <Wip
       title="Championne"
       details={`Producing a few songs for Rennes-based artist Mathilde Lejas. Amazing songwriter.`}
     />
     <Wip
+      title="Wonderboy"
+      details={`Just mixed some new song for the Rennes-based artist Wonderboy.`}
+    />
+    <Wip
       title="Neptune in June"
       details={`Producing an album for a brand new project called Neptune in June (formerly known as La Houle). If you like lo-fi pop music and distortion, stay tuned.`}
     />
     <Wip
-      title="Wonderboy"
-      details={`About to mix some new songs of the Rennes-based artist Wonderboy.`}
-    />
-    <Wip
       title="Edinburgh of the Seven Seas"
-      details={`Producing, recording, mixing and playing some instruments on and for the upcoming Edinburgh of the Seven Seas album. He is an island and also a good friend of us.`}
+      details={`Producing, recording, mixing and playing some instruments on and for the Edinburgh of the Seven Seas debut album. He is a gem and also a good friend of ours.`}
     />
     <Wip
       title="Mix With The Masters #2"
-      details={`Just came back from a one-week seminar at the La Fabrique studio with Tchad Blake. Previous one was with Shawn Everett. Both were opportunities to learn from amazing people.`}
+      details={`Just got back from a one-week seminar at La Fabrique studio with Tchad Blake. Also went to their previous one was with Shawn Everett. They were such great opportunities to learn from amazing people.`}
     />
     <Wip
       title="New studio set up"
-      details={`Just found a new home near Nantes. Sharing this studio with Hugo from Von Pariahs, Happier, ...`}
+      details={`Just got our hands on a new home around Nantes. Sharing the studio with Hugo from Von Pariahs, Happier, ...`}
     />
   </div>
 )
